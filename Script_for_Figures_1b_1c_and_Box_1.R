5e0e923eea15a48036bfdad66501a10007805e36
YasinEl <42336795+YasinEl@users.noreply.github.com>
2022-10-17 19:15:52 -0700

    Add script to produce figures

---
+++ Script_for_Figures_1b_1c_and_Box_1.R
@@ -0,0 +1,62 @@
+#Install/Load necessary packages
+if("data.table" %in% rownames(installed.packages()) == FALSE){
+  install.packages("data.table")
+}
+if("ggplot2" %in% rownames(installed.packages()) == FALSE){
+  install.packages("ggplot2")
+}
+if("ggforce" %in% rownames(installed.packages()) == FALSE){
+  install.packages("ggforce")
+}
+if("ggVennDiagram" %in% rownames(installed.packages()) == FALSE){
+  install.packages("ggVennDiagram")
+}
+
+library(data.table)
+library(ggVennDiagram)
+library(ggplot2)
+library(ggforce)
+
+#####################
+#Generate Figure 1b
+#####################
+dt = fread("Table_for_figures_1b_and_1c.csv")
+dbs_li = list(HMDB = dt[DB == "HMDB"]$SMILES, LIPIDMAPS = dt[DB == "LIPIDMAPS"]$SMILES, BILELIB19 = dt[DB == "BILELIB19"]$SMILES)
+
+ggVennDiagram(dbs_li, label_alpha = 0)  +
+  scale_fill_gradientn(colours = c('#8EBAD9', '#EA9293')) + 
+  ggtitle(paste0(c(length(unique(dt$SMILES)), "unique bile acid SMILES"), collapse = " ")) +
+  theme(text = element_text(size = 9)) +
+  theme(plot.title = element_text(hjust = 0.5), legend.position = "none")
+
+
+#####################
+#Generate Figure 1c
+#####################
+dt = fread("Table_for_figures_1b_and_1c.csv")
+dt = dt[abs(delta_mz) > 40, .(DB = ifelse(length(unique(DB[!is.na(DB)])) > 1, "Multiple", unique(DB[!is.na(DB)])), delta_mz = delta_mz[1]), by = .(alk_mods, ring_mods)]
+
+ggplot(dt) +
+  xlab("Oberservable delta mz from unconjugated bile acid") +
+  geom_vline(aes(xintercept = round(delta_mz, 3), color = DB), dt, lwd = 1) +
+  scale_color_manual(values=c('#8EBAD9', '#EA9293', '#FFBE86', "#C9B2DD")) +
+  labs(color = "", text = element_text(size = 9)) +
+  ggforce::facet_zoom(xlim = c(40, 220), zoom.size = 1) 
+
+
+#####################
+#Generate Figure Box1
+#####################
+dt = fread("Table_for_figure_Box1.csv")
+
+ggplot(dt, aes(x = reorder(as.factor(name), concentration_mean), y = concentration_mean, fill = specimen)) +
+  geom_bar(stat="identity", position= position_dodge(preserve = "single")) +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), text = element_text(size = 10)) +
+  scale_fill_manual(values=c('#8EBAD9', '#EA9293')) +
+  labs(fill = "") +
+  xlab('HMDB name') +
+  ylab('Concentration (micromol/L)') + 
+  geom_errorbar(aes(ymin=concentration_min, ymax=concentration_max), lwd = .02, width=.2, position=position_dodge(.9, preserve = "single")) +
+  facet_zoom(ylim = c(0,60), zoom.size = 1)
+
